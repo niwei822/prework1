daa68372e5cc2e109ece82cdf595587611b79cbf
niwei <69645683+niwei822@users.noreply.github.com>
2022-11-07 06:07:48 +0000

    add prework1

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print("Hello")
-bunbun = input()
-print(bunbun + " is my favorite ice cream flavor!") 
+firstName = input("What’s your first name?: ")
+lastName = input("What’s your last name?: ")
+color = input("What’s your favorite color?: ")
+pets = input("How many pets do you have?: ")
+
+message = (
+    f"Tell us your first name: {firstName}. "\
+    f"Tell us your last name: {lastName}. "\
+    f"Tell us your favorite color: {color}. "\
+    f"Tell us how many pets you have: {pets}. "\
+    f"Great! So your name is {firstName} {lastName}, your favorite color is {color} and you have {pets} pets. Nice to meet you! " )
+print(message)
